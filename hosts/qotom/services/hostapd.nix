366bdd7a5fc3cbf1c7beb7b443c17dc220d2ea5b
frantathefranta <fb@franta.us>
2025-05-21 11:21:19 -0400

    Added kea-dhcp to qotom

--- hosts/qotom/services/hostapd.nix
+++ hosts/qotom/services/hostapd.nix
@@ -21,7 +21,37 @@
         };
       };
     };
+    kea.dhcp4 = {
+      enable = true;
+      settings = {
+        interfaces-config = {
+          interfaces = [
+            "wlp2s0"
+          ];
+        };
+        lease-database = {
+          name = "/var/lib/kea/dhcp4.leases";
+          persist = true;
+          type = "memfile";
+        };
+        rebind-timer = 2000;
+        renew-timer = 1000;
+        subnet4 = [
+          {
+            id = 1;
+            pools = [
+              {
+                pool = "172.32.254.16 - 172.32.254.31";
+              }
+            ];
+            subnet = "172.32.254.0/27";
+          }
+        ];
+        valid-lifetime = 4000;
+      };
+    };
   };
+
   sops.secrets.wpa-password = {
     sopsFile = ../secrets.yaml;
   };

4d8b0bc124facb3fcf2ed03addcdea9c916cf999
frantathefranta <fb@franta.us>
2025-05-23 15:23:07 -0400

    Updated nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My nix config";
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # Nixpkgs
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
@@ -16,7 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Emacs

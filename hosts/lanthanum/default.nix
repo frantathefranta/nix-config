a01d7fa2b88cc00251170a9e5283c3f18e902ee1
frantathefranta <fb@franta.us>
2025-07-17 11:17:03 -0400

    Removing common-gpu-nvidia module

--- hosts/lanthanum/default.nix
+++ hosts/lanthanum/default.nix
@@ -6,7 +6,7 @@
 {
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
-    inputs.hardware.nixosModules.common-gpu-nvidia
+    # inputs.hardware.nixosModules.common-gpu-nvidia
     inputs.hardware.nixosModules.common-pc-ssd
     ./services
     ./hardware-configuration.nix

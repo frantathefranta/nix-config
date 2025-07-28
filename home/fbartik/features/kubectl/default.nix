de73d5eec2cb7f9fc4bdbd601c8b36f7a4ebccc9
frantathefranta <fb@franta.us>
2025-07-28 10:30:27 -0400

    kube environment tweaks

--- home/fbartik/features/kubectl/default.nix
+++ home/fbartik/features/kubectl/default.nix
@@ -1,16 +1,71 @@
 { pkgs, ... }:
 
 {
-  programs.k9s.enable = true;
+  programs.k9s = {
+    enable = true;
+    package = pkgs.unstable.k9s;
+    plugin = {
+      reconcile-git = {
+        shortCut = "Shift-R";
+        confirm = false;
+        description = "Flux reconcile";
+        scopes = [ "gitrepositories" ];
+        command = "bash";
+        background = false;
+        args = [
+          "-c"
+          ">-"
+          "flux"
+          "--context $CONTEXT"
+          "reconcile source git"
+          "-n $NAMESPACE $NAME"
+          "| less -K"
+        ];
+      };
+      reconcile-hr = {
+        shortCut = "Shift-R";
+        confirm = false;
+        description = "Flux reconcile";
+        scopes = [ "helmreleases" ];
+        command = "bash";
+        background = false;
+        args = [
+          "-c"
+          ">-"
+          "flux"
+          "--context $CONTEXT"
+          "reconcile helmrelease"
+          "-n $NAMESPACE $NAME"
+          "| less -K"
+        ];
+      };
+      stern = {
+        shortCut = "Ctrl-Y";
+        confirm = false;
+        description = "Logs <stern>";
+        scopes = [ "pods" ];
+        command = "${pkgs.stern}/bin/stern";
+        background = false;
+        args = [
+          "--tail"
+          "50"
+          "$FILTER"
+          "-n $NAMESPACE"
+          "--context $CONTEXT"
+        ];
+      };
+    };
+  };
   home.packages = with pkgs; [
-    fluxcd
+    unstable.fluxcd
+    unstable.talosctl
+    stern # Logs
     go-task
     jq
     kubectl
     kubernetes-helm
     kubecolor
     kustomize
-    talosctl
   ];
   programs.fish.shellAbbrs = {
     k = {

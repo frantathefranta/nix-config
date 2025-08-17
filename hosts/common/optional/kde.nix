140fdd0aad9b4c738fe872b4409d4e137e5e9109
frantathefranta <fb@franta.us>
2025-08-17 14:48:56 -0400

    Added kdePackages

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -19,5 +19,6 @@
     discover # Optional: Install if you use Flatpak or fwupd firmware update sevice
     ksystemlog
     sddm-kcm # Configuration module for SDDM
+    krohnkite # Dynamic tiling manager
   ];
 }

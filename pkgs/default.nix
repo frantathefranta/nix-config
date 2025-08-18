bdda5667331f1b16ac10b9c29ccd7bf5aa9b3b9f
frantathefranta <fb@franta.us>
2025-08-18 09:56:44 -0400

    Switching to upstream conman

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,5 +6,5 @@ rec {
   # Custom packages, that can be defined similarly to ones from nixpkgs
   # You can build them using 'nix build .#example'
   # example = pkgs.callPackage ./example { };
-  conman = pkgs.callPackage ./conman { };
+  # conman = pkgs.callPackage ./conman { };
 }

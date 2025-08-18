9638fee8b6dfe0dbfa20b8d6dbb33b14857a37ae
frantathefranta <fb@franta.us>
2025-08-18 10:32:53 -0400

    Tweaks in global settings

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -3,32 +3,34 @@
   inputs,
   outputs,
   pkgs,
+  lib,
+  config,
   ...
-}: {
-  imports =
-    [
-      inputs.home-manager.nixosModules.home-manager
-      # ./acme.nix
-      # ./auto-upgrade.nix
-      ./fish.nix
-      ./locale.nix
-      ./nh.nix
-      ./nix.nix
-      ./openssh.nix
-      # ./optin-persistence.nix
-      # ./podman.nix
-      ./sops.nix
-      # ./ssh-serve-store.nix
-      # ./steam-hardware.nix
-      # ./systemd-initrd.nix
-      # ./tailscale.nix
-      # ./gamemode.nix
-      ./nix-ld.nix
-      ./prometheus-node-exporter.nix
-      # ./kdeconnect.nix
-      # ./upower.nix
-    ]
-    ++ (builtins.attrValues outputs.nixosModules);
+}:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    # ./acme.nix
+    # ./auto-upgrade.nix
+    ./fish.nix
+    ./locale.nix
+    ./nh.nix
+    ./nix.nix
+    ./openssh.nix
+    # ./optin-persistence.nix
+    # ./podman.nix
+    ./sops.nix
+    # ./ssh-serve-store.nix
+    # ./steam-hardware.nix
+    # ./systemd-initrd.nix
+    # ./tailscale.nix
+    # ./gamemode.nix
+    ./nix-ld.nix
+    ./prometheus-node-exporter.nix
+    # ./kdeconnect.nix
+    # ./upower.nix
+  ]
+  ++ (builtins.attrValues outputs.nixosModules);
 
   home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = {
@@ -43,16 +45,15 @@
   };
 
   # Fix for qt6 plugins
-  # TODO: maybe upstream this?
   environment.profileRelativeSessionVariables = {
-    QT_PLUGIN_PATH = ["/lib/qt-6/plugins"];
+    QT_PLUGIN_PATH = [ "/lib/qt-6/plugins" ];
   };
 
   environment.systemPackages = with pkgs; [
     neovim
   ];
 
-  hardware.enableRedistributableFirmware = true;
+  hardware.enableRedistributableFirmware = lib.mkIf (config.services.qemuGuest.enable != true) true;
   networking = {
     firewall.enable = true;
     domain = "infra.franta.us";

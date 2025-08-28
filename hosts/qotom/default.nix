db6fdf3fbcf552885bd0c9dcb6fff8353c2fe9d4
frantathefranta <fb@franta.us>
2025-08-28 10:47:23 -0400

    Deadnix clean-up

--- hosts/qotom/default.nix
+++ hosts/qotom/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ inputs, ... }:
 {
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
--- overlays/default.nix
+++ overlays/default.nix
@@ -2,12 +2,12 @@
 { inputs, ... }:
 {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: prev: import ../pkgs { pkgs = final; };
+  additions = final: _prev: import ../pkgs { pkgs = final; };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
-  modifications = final: prev: {
+  modifications = _final: _prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });

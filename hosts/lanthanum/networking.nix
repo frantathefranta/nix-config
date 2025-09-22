057d8a75ea2999572ff41c107ec746237805cb89
frantathefranta <fb@franta.us>
2025-09-21 23:30:27 -0400

    Added BGP to lanthanum

--- hosts/lanthanum/networking.nix
+++ hosts/lanthanum/networking.nix
@@ -2,6 +2,7 @@
 {
   networking = {
     firewall.interfaces.eno1.allowedTCPPorts = [
+      179
       22000
     ];
     firewall.interfaces.eno1.allowedUDPPorts = [
@@ -65,6 +66,7 @@
   };
   services.frr = {
     bfdd.enable = true;
+    bgpd.enable = true;
     ospfd.enable = true;
     ospf6d.enable = true;
     config = ''
@@ -89,6 +91,17 @@
         max-metric router-lsa administrative
       router ospf6
         ospf6 router-id 10.0.0.99
+      router bgp 65033
+        bgp router-id 10.0.0.99
+        bgp log-neighbor-changes
+        no bgp ebgp-requires-policy
+        no bgp hard-administrative-reset
+        no bgp graceful-restart notification
+        no bgp network import-check
+        neighbor fabric peer-group
+        neighbor fabric update-source 10.0.0.99
+        neighbor 10.0.0.2 remote-as 65033
+        neighbor 10.0.0.2 peer-group fabric
       route-map SETSOURCE permit 10
         set src 10.0.0.99
       ip protocol ospf route-map SETSOURCE

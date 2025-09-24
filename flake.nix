bd98d69e438e4f9e2a94d7c583e6da6568c10b32
frantathefranta <fb@franta.us>
2025-09-23 22:23:01 -0400

    Removed work-vm for now

--- flake.nix
+++ flake.nix
@@ -103,12 +103,12 @@
             ./hosts/qotom
           ];
         };
-        work-vm = lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [
-            ./hosts/work-vm
-          ];
-        };
+        # work-vm = lib.nixosSystem {
+        #   specialArgs = { inherit inputs outputs; };
+        #   modules = [
+        #     ./hosts/work-vm
+        #   ];
+        # };
         nixos-firewall = lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [

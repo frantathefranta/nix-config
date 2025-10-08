a1b2b447eae6695fee495f570367a6410e9a6d56
frantathefranta <fb@franta.us>
2025-10-08 12:31:51 -0400

    Added initial PowerDNS config

--- hosts/qotom/services/default.nix
+++ hosts/qotom/services/default.nix
@@ -3,6 +3,7 @@
     ./hostapd.nix
     ./syslog.nix
     ./conman.nix
+    ./powerdns.nix
   ];
   services = {
     lldpd.enable = true;
---
+++ hosts/qotom/services/powerdns.nix
@@ -0,0 +1,5 @@
+{
+  services.powerdns = {
+    enable = true;
+  }; 
+}

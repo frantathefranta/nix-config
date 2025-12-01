90298656410cefba2edb3dbd6602df897458e668
frantathefranta <fb@franta.us>
2025-12-01 10:44:57 -0500

    Updated flake inputs to 25.11

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     # Nixpkgs
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:

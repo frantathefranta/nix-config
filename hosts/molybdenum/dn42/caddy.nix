97647ba0092ddaa92f3e38b42a1771dd13e0e0a8
frantathefranta <fb@franta.us>
2025-12-08 12:33:42 -0500

    Caddy tweaks

--- hosts/molybdenum/dn42/caddy.nix
+++ hosts/molybdenum/dn42/caddy.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
 {
   services.caddy = {
     enable = true;
+    globalConfig = ''
+      acme_ca https://acme.burble.dn42/v1/dn42/acme/directory
+      acme_ca_root ${pkgs.dn42-cacert}/etc/ssl/certs/dn42-ca.crt
+    '';
     extraConfig = ''
       lg.franta.dn42
-      reverse_proxy :5000
+      reverse_proxy [fdb7:c21f:f30f::1]:5000
     '';
   };
 }

2d4d61a481ead5d65c1fc62c7374b8d05e8bcc8c
frantathefranta <fb@franta.us>
2025-12-22 09:29:00 -0500

    Trying gobgp

--- hosts/qotom/default.nix
+++ hosts/qotom/default.nix
@@ -1,8 +1,14 @@
-{ config, lib, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 {
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
     inputs.hardware.nixosModules.common-pc-ssd
+    inputs.gobgp.nixosModules.gobgp
     ./services
     ./hardware-configuration.nix
 
@@ -16,6 +22,20 @@
     hostName = "qotom";
     useDHCP = false;
     enableIPv6 = true;
+    interfaces.lo = {
+      ipv4.addresses = [
+        {
+          address = "10.0.10.10";
+          prefixLength = 32;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2600:1702:6630:3fec::10:10";
+          prefixLength = 128;
+        }
+      ];
+    };
     interfaces.enp1s0 = {
       ipv4.addresses = [
         {
@@ -58,9 +78,9 @@
   services.resolved.extraConfig = ''
     DNSStubListener=no
   '';
-   
+
   # The networking.nameservers get prepended to /etc/resolv.conf, defeating the purpose of selecting a DNS server per domain
-  networking.nameservers = [];
+  networking.nameservers = [ ];
 
   services.dnsmasq = {
     enable = true;
@@ -93,7 +113,9 @@
   };
   systemd.services.systemd-networkd.serviceConfig = {
     LoadCredential = [
-      "network.wireguard.private.50-wg_mikrotik:${config.sops.secrets."wireguard/mikrotik-private-key".path}"
+      "network.wireguard.private.50-wg_mikrotik:${
+        config.sops.secrets."wireguard/mikrotik-private-key".path
+      }"
     ];
   };
   systemd.network.netdevs."50-wg_mikrotik" = {
@@ -107,7 +129,7 @@
     };
     wireguardPeers = [
       {
-        Endpoint = "10.32.10.108:44068";
+        Endpoint = "mikrotik.eu.franta.us:44068";
         PersistentKeepalive = 5;
         PublicKey = "E/zt3wlE3yKum2CBlakSCUXGTTLZOoI4giAlKOCk0mY=";
         AllowedIPs = [
@@ -121,35 +143,104 @@
     matchConfig.Name = "wg_mikrotik";
     addresses = [
       {
-        Address = "169.254.1.1/16";
-        Peer = "169.254.1.2/16";
+        Address = "fe80::aaaa:1/64";
+        Peer = "fe80::aaaa:2/64";
       }
+      # {
+      #   Address = "169.254.10.1/30";
+      #   Peer = "169.254.10.2/30";
+      # }
     ];
     networkConfig = {
       LinkLocalAddressing = false;
     };
-  };  sops.secrets = {
+  };
+  sops.secrets = {
     "wireguard/mikrotik-private-key" = {
       sopsFile = ./secrets.yaml;
     };
   };
-  services.frr = {
-    bgpd.enable = true;
-    config = ''
-      router bgp 65032
-        no bgp ebgp-requires-policy
-        bgp router-id 10.32.10.10
-        neighbor 2600:1702:6630:3fed::1 remote-as 65033
-        address-family ipv6
-          network fdb7:c21f:f30f:10::10/128 
-          neighbor 2600:1702:6630:3fed::1 activate
-      ipv6 prefix-list dn42_ips seq 10 permit fd00::/8 ge 48
-      route-map correct_src permit 1
-        match ipv6 address prefix-list dn42_ips
-        set src fdb7:c21f:f30f:10::10
-      ipv6 protocol bgp route-map correct_src
-    '';
+  services.gobgpd = {
+    enable = true;
+    zebra = true;
+    validateConfig = false;
+    config = {
+      global = {
+        as = 65032;
+        router-id = "10.0.10.10";
+      };
+      neighbors = {
+        "arista" = {
+          peer-as = 65033;
+          neighbor-address = "fe80::464c:a8ff:fede:3cf7%enp1s0";
+          afi-safis.ipv4-unicast = { };
+          afi-safis.ipv6-unicast = { };
+          apply-policy.import-policy-list = [
+            "allow-dn42"
+          ];
+      };
+      };
+      defined-sets = {
+        prefix-sets = {
+          "dn42" = {
+            prefix-list = [
+              {
+                ip-prefix = "fd00::/8";
+                masklength-range = "48..128";
+              }
+            ];
+          };
+        };
+      };
+      policy-definitions = {
+        "allow-dn42" = {
+          statements = {
+            "match-prefix-set" = {
+              actions.route-disposition = "accept-route";
+              conditions = {
+                match-prefix-set = {
+                  prefix-set = "dn42";
+                  # match-set-options = "any";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
   };
+  # services.frr = {
+  #   bgpd.enable = true;
+  #   config = ''
+  #     router bgp 65032
+  #       no bgp ebgp-requires-policy
+  #       no bgp network import-check
+  #       bgp router-id 10.32.10.10
+  #       neighbor 2600:1702:6630:3fed::1 remote-as 65033
+  #       neighbor fe80::aaaa:2 remote-as 65534
+  #       neighbor fe80::aaaa:2 description mikrotik
+  #       neighbor fe80::aaaa:2 interface wg_mikrotik
+  #       neighbor fe80::aaaa:2 capability extended-nexthop
+  #       address-family ipv4 unicast
+  #         network 10.0.10.10/32
+  #       address-family ipv6
+  #         network fdb7:c21f:f30f:10::10/128
+  #         network 2600:1702:6630:3fec::10:10/128
+  #         neighbor 2600:1702:6630:3fed::1 activate
+  #         neighbor 2600:1702:6630:3fed::1 route-map correct_src in
+  #         neighbor fe80::aaaa:2 activate
+  #         neighbor fe80::aaaa:2 route-map to_mikrotik out
+  #     ipv6 prefix-list dn42_ips seq 10 permit fd00::/8 ge 48
+  #     ipv6 prefix-list loopback_ips seq 10 permit 2600:1702:6630:3fec::10:10/128
+  #     route-map to_mikrotik deny 1
+  #       match ipv6 address prefix-list dn42_ips
+  #     route-map to_mikrotik permit 2
+  #       match ipv6 address prefix-list loopback_ips
+  #     route-map correct_src permit 1
+  #       match ipv6 address prefix-list dn42_ips
+  #       set src fdb7:c21f:f30f:10::10
+  #   '';
+  # };
   services.prometheus.exporters.node = {
     listenAddress = "10.32.10.10";
   };

2e17e3636c0700cf9ba07eb492445a5458b9c542
Franta Bartik <fb@franta.us>
2026-02-27 16:28:18 -0500

    Tweaked caddy config on molybdenum

--- hosts/molybdenum/dn42/caddy.nix
+++ hosts/molybdenum/dn42/caddy.nix
@@ -6,9 +6,16 @@
       acme_ca https://acme.burble.dn42/v1/dn42/acme/directory
       acme_ca_root ${pkgs.dn42-cacert}/etc/ssl/certs/dn42-ca.crt
     '';
-    extraConfig = ''
-      lg.franta.dn42
-      reverse_proxy [fdb7:c21f:f30f::1]:5000
-    '';
+    virtualHosts."lg.franta.dn42" = {
+      extraConfig = ''
+        reverse_proxy [fdb7:c21f:f30f::1]:5000
+      '';
+    };
+    virtualHosts."franta.dn42" = {
+      extraConfig = ''
+        root * /var/www/public
+        file_server browse
+      '';
+    };
   };
 }

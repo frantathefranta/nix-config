eaa431a9eef17da7dad14d80de401034133d8d00
Franta Bartik <fb@franta.us>
2026-03-06 16:16:10 -0500

    Moving to RPKI servers instead of scripted systemd timer

--- hosts/molybdenum/dn42/bird2.nix
+++ hosts/molybdenum/dn42/bird2.nix
@@ -6,42 +6,11 @@
 }:
 
 let
-  script = pkgs.writeShellScriptBin "update-roa" ''
-    mkdir -p /etc/bird/
-    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf
-    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf
-    ${pkgs.bird3}/bin/birdc c 
-    ${pkgs.bird3}/bin/birdc reload filters in all
-  '';
   bgp = import peers/bgp.nix { };
   hostIPv4 = builtins.elemAt config.networking.interfaces.ens18.ipv4.addresses 0;
   address = hostIPv4.address;
 in
 {
-
-  systemd.timers.dn42-roa = {
-    description = "Trigger a ROA table update";
-
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitInactiveSec = "1h";
-      Unit = "dn42-roa.service";
-    };
-
-    wantedBy = [ "timers.target" ];
-    before = [ "bird.service" ];
-  };
-
-  systemd.services = {
-    dn42-roa = {
-      after = [ "network.target" ];
-      description = "DN42 ROA Updated";
-      serviceConfig = {
-        # Type = "one-shot";
-        ExecStart = "${script}/bin/update-roa";
-      };
-    };
-  };
   services = {
     bird-lg = {
       # package = unstable.bird-lg;
--- hosts/nix-hetzner/dn42/bird.nix
+++ hosts/nix-hetzner/dn42/bird.nix
@@ -1,39 +1,9 @@
 { pkgs, lib, ... }:
 
 let
-  script = pkgs.writeShellScriptBin "update-roa" ''
-    mkdir -p /etc/bird/
-    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf
-    ${pkgs.curl}/bin/curl -sfSLR {-o,-z}/etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf
-    ${pkgs.bird3}/bin/birdc c 
-    ${pkgs.bird3}/bin/birdc reload filters in all
-  '';
   bgp = import peers/bgp.nix { };
 in
 {
-  systemd.timers.dn42-roa = {
-    description = "Trigger a ROA table update";
-
-    timerConfig = {
-      OnBootSec = "5m";
-      OnUnitInactiveSec = "1h";
-      Unit = "dn42-roa.service";
-    };
-
-    wantedBy = [ "timers.target" ];
-    before = [ "bird.service" ];
-  };
-
-  systemd.services = {
-    dn42-roa = {
-      after = [ "network.target" ];
-      description = "DN42 ROA Updated";
-      serviceConfig = {
-        # Type = "one-shot";
-        ExecStart = "${script}/bin/update-roa";
-      };
-    };
-  };
   services = {
     bird-lg = {
       proxy = {
@@ -45,14 +15,6 @@ in
         ];
         birdSocket = "/var/run/bird/bird.ctl";
       };
-      # frontend = {
-      #   enable = true;
-      #   servers = [
-      #     "us-cmh"
-      #   ];
-      #   domain = "franta.dn42";
-      #   listenAddresses = "${address}:5000";
-      # };
     };
     bird = {
       enable = true;

be77dcf697e99beba9654b9e8e31de687fad2f47
Franta Bartik <fb@franta.us>
2026-03-16 14:02:12 -0400

    Added an After= to conmand service

--- hosts/qotom/services/conman.nix
+++ hosts/qotom/services/conman.nix
@@ -4,6 +4,8 @@
     enable = true;
     configFile = config.sops.secrets."conman.conf".path;
   };
+  # This will make conman wait until USB serial device is ready
+  systemd.services.conmand.after = [ "sys-devices-pci0000:00-0000:00:14.0-usb2-2-4-2-4:1.0-ttyUSB0-tty-ttyUSB0.device" ];
   sops.secrets."conman.conf" = {
     sopsFile = ../secrets.yaml;
   };

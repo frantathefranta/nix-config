9af27a3c0179f073ac28d6e8bfb26d2aa2d92e3f
Franta <64412753+frantathefranta@users.noreply.github.com>
2026-03-17 14:29:54 -0400

    Nftables work (#651)
    * WIP changes
    
    * Added more email secrets
    
    * Removed aerc
    
    * Updated zone-rules
    
    * Enabling firewall
    
    * Disabling server import
    
    * Fixing up sysctl
    
    * Forgot a closing bracket
    
    * Forgot to comment these out
    
    * Added a rule for mgmt interface
    
    * More mgmt rules
    
    * Does this enable ping
    
    * Testing define for nftables
    
    * Added IPv6 directives
    
    * Removed ip specific directives
    
    * Re-adding ip specific directives but changing family of table
    
    * Enabling IPv6 ICMP
    
    * Misc tweaks

--- hosts/nixos-firewall/services/default.nix
+++ hosts/nixos-firewall/services/default.nix
@@ -2,6 +2,12 @@
   imports = [
     ./network.nix
     ./lldpd.nix
-    # ./firewall
+    ./firewall
+    # TODO: ./dns.nix
+    # TODO: ./dhcp.nix # https://github.com/budimanjojo/nix-config/blob/main/system/hosts/budimanjojo-firewall/_modules/services/kea/default.nix
+    # TODO: ./radvd.nix
+    # TODO: ./ntp.nix
+    # TODO: ./routing.nix
+    # TODO: ./qos.nix # https://github.com/budimanjojo/nix-config/blob/main/system/hosts/budimanjojo-firewall/_modules/services/fireqos/default.nix
   ];
 }

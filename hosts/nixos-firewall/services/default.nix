88631f6febea20c17631953b2c5af31b87c761a0
Franta Bartik <fb@franta.us>
2026-03-17 14:57:19 -0400

    Initial powerdns commit

--- hosts/nixos-firewall/services/default.nix
+++ hosts/nixos-firewall/services/default.nix
@@ -3,7 +3,7 @@
     ./network.nix
     ./lldpd.nix
     ./firewall
-    # TODO: ./dns.nix
+    ./powerdns.nix
     # TODO: ./dhcp.nix # https://github.com/budimanjojo/nix-config/blob/main/system/hosts/budimanjojo-firewall/_modules/services/kea/default.nix
     # TODO: ./radvd.nix
     # TODO: ./ntp.nix
---
+++ hosts/nixos-firewall/services/powerdns.nix
@@ -0,0 +1,70 @@
+# Taken from https://github.com/budimanjojo/nix-config/blob/main/system/hosts/budimanjojo-firewall/_modules/services/powerdns/default.nix
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  directory = "/var/lib/pdns";
+  user = "pdns";
+  group = "pdns";
+in
+{
+  systemd.tmpfiles.rules = [ "d ${directory} 0750 ${user} ${group}" ];
+
+  sops.secrets."powerdns/env" = {
+    sopsFile = ../secrets.yaml;
+    owner = user;
+    group = group;
+  };
+
+  services.powerdns = {
+    enable = true;
+    extraConfig = ''
+      local-address=10.0.10.1:53,10.32.10.230:53,[2600:1702:6630:3fed:10:32:10:230]:53 # TODO: Add lan0 IPv6 address
+      launch=gsqlite3
+      gsqlite3-database=${directory}/pdns.sqlite3
+      dnsupdate=yes
+      # allow-dnsupdate-from=192.168.10.1/32
+      default-soa-content=@ gateway.franta.us. 0 7200 3600 120960 3600
+    '';
+    secretFile = config.sops.secrets."powerdns/env".path;
+  };
+
+  systemd.services.pdns.serviceConfig = {
+    # powerdns doesn't create the sqlite database for us
+    # so we gotta either do it manually one-off or do the below to ensure it's created
+    # if the file is missing before service start
+    ExecStartPre = lib.mkBefore [
+      (pkgs.writeScript "pdns-sqlite-init.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        pdns_folder="${directory}"
+        echo "INIT: checking if pdns sqlite exists"
+        if [ ! -f "${directory}/pdns.sqlite3" ]; then
+          echo "INIT: no sqlite db found, initializing from pdns pkgs schema..."
+          ${pkgs.sqlite}/bin/sqlite3 "${directory}/pdns.sqlite3" < "${pkgs.pdns}/share/doc/pdns/schema.sqlite3.sql"
+          ${pkgs.busybox}/bin/chown pdns:pdns ${directory}/pdns.sqlite3
+        fi
+
+        # exit successfully
+        exit 0
+      '')
+    ];
+    ExecStartPost = (
+      pkgs.writeScript "pdns-ddns-setup.sh" ''
+        #!${pkgs.bash}/bin/bash
+
+        cmd=${pkgs.pdns}/bin/pdnsutil
+
+        $cmd create-zone franta.us. || true
+        $cmd create-zone 10.in-addr.arpa. || true
+
+        $cmd import-tsig-key kea hmac-sha512 $KEA_TSIG_KEY
+        $cmd set-meta franta.us. TSIG-ALLOW-DNSUPDATE kea
+        $cmd set-meta 10.in-addr.arpa. TSIG-ALLOW-DNSUPDATE kea
+      ''
+    );
+  };
+}

006b6651d9374f123bfcaa1bd1c89b3f8a89e77b
Franta Bartik <fb@franta.us>
2026-03-27 22:37:06 -0400

    Claude-code fixes

--- home/fbartik/features/productivity/claude-code.nix
+++ home/fbartik/features/productivity/claude-code.nix
@@ -3,5 +3,16 @@
   programs.claude-code = {
     enable = true;
     package = pkgs.unstable.claude-code;
+    settings = {
+      sandbox = {
+        enabled = true;
+        filesystem = {
+          allowRead = [ "." ];
+          denyRead = [ "~/" ];
+          allowWrite = [ "." ];
+          denyWrite = [ "/" ];
+        };
+      };
+    };
   };
 }

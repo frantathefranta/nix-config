7a6ba4db1a81f5ae9e86767a1d7b16fb2ad3d436
Franta Bartik <fb@franta.us>
2026-04-06 15:14:59 -0400

    Temporary workaround since direnv
    Direnv in cache.nixos.org still has vendor_conf.d/direnv.fish somehow

--- home/fbartik/features/cli/direnv.nix
+++ home/fbartik/features/cli/direnv.nix
@@ -1,7 +1,16 @@
-{
+{ pkgs, ... }: {
   programs.direnv = {
-    # enable = true;
+    enable = true;
+    enableFishIntegration = false;
     nix-direnv.enable = true;
+    # nixpkgs PR#475992 added postInstall = "rm -rf $out/share/fish" but
+    # direnv sets installPhase as a variable, so Nix evals it directly and
+    # never calls runHook postInstall. Append the rm to installPhase itself.
+    package = pkgs.direnv.overrideAttrs (old: {
+      installPhase = old.installPhase + ''
+        rm -rf "$out/share/fish"
+      '';
+    });
   };
   programs.direnv-instant = {
     enable = true;

a968fc6c93985c012777386788ef31e39dbff481
Franta Bartik <fb@franta.us>
2026-04-20 16:32:25 -0400

    Local NTP server changed

--- hosts/common/global/time.nix
+++ hosts/common/global/time.nix
@@ -1,7 +1,7 @@
 { lib, config, ... }:
 {
   services.timesyncd = {
-    servers = lib.mkIf (config.networking.domain == "infra.franta.us") ["192.168.247.254"];
+    servers = lib.mkIf (config.networking.domain == "infra.franta.us") ["time.infra.franta.us"];
     # fallbackServers are 0.nixos.pool.ntp.org 1.nixos.pool.ntp.org 2.nixos.pool.ntp.org 3.nixos.pool.ntp.org
   };
 }

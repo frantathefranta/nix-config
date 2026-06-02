7a7881c15c126e7090dae5a9c3a2fc6999fd92ee
Franta Bartik <fb@franta.us>
2026-06-01 22:02:32 -0400

    Cleaned-up wireless.nix

--- hosts/silicium/services/wireless.nix
+++ hosts/silicium/services/wireless.nix
@@ -4,17 +4,6 @@
   ...
 }:
 
-let
-  wpaActionScript = pkgs.writeShellScript "wpa-action" ''
-    case "$2" in
-      CONNECTED)
-        sleep 2
-        ${pkgs.systemd}/bin/networkctl reconfigure "$1"
-        ;;
-    esac
-  '';
-in
-
 {
   hardware.bluetooth = {
     enable = true;
@@ -37,23 +26,7 @@ in
       };
     };
     allowAuxiliaryImperativeNetworks = false;
-    # extraConfig = ''
-    #   ctrl_interface=DIR=/run/wpa_supplicant GROUP=${config.users.groups.network.name}
-    #   update_config=1
-    # '';
   };
   # Ensure group exists
   users.groups.network = { };
-  # systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
-
-  # systemd.services.wpa-action = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "wpa_supplicant.service" ];
-  #   requires = [ "wpa_supplicant.service" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.wpa_supplicant}/bin/wpa_cli -i wlp3s0 -a ${wpaActionScript}";
-  #     Restart = "on-failure";
-  #     RestartSec = "5s";
-  #   };
-  # };
 }

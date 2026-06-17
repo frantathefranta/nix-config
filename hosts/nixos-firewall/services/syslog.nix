a6dc1717d0a5a61ac3208ab71567808a50f0e037
Franta Bartik <fb@franta.us>
2026-06-17 15:10:22 -0400

    Elementary logrotate

--- hosts/nixos-firewall/services/syslog.nix
+++ hosts/nixos-firewall/services/syslog.nix
@@ -1,6 +1,13 @@
+{ pkgs, ... }:
 {
   services.rsyslogd = {
     enable = true;
+    # Rainerscript doesn't work with current NixOS config because supplied nixos.conf still has some old syntax settings
+    # extraConfig = ''
+    #   module(load="imudp")
+    #   input(type="imudp" port="514")
+    #   template(name="RSYSLOG_FileFormat)
+    # '';
     extraConfig = ''
       $ModLoad imudp
       $UDPServerRun 514
@@ -10,9 +17,25 @@
       :source, isequal, "last" stop
     '';
   };
-
+  systemd = {
+    services.rsyslog-cleanup = {
+      after = [ "multi-user.target" ];
+      description = "Delete log files older than 30 days";
+      script = "${pkgs.findutils}/bin/find /var/spool/rsyslog/ -daystart -mtime +30 -type f -exec rm -f {} \\;";
+      startAt = "weekly";
+      serviceConfig.Type = "oneshot";
+    };
+    timers.rsyslog-cleanup = {
+      timerConfig = {
+        Persistent = true;
+      };
+    };
+  };
   networking.nftables.firewall.rules.allow_syslog = {
-    from = [ "lab_space" ];
+    from = [
+      "lab_space"
+      "local_interfaces"
+    ];
     to = [ "fw" ];
     allowedUDPPorts = [ 514 ];
   };

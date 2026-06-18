faa7f9c638f68680af3b389c9b97cb63ea590d41
Franta Bartik <fb@franta.us>
2026-06-18 16:54:12 -0400

    Updating akeyless package

--- pkgs/akeyless-cli/default.nix
+++ pkgs/akeyless-cli/default.nix
@@ -1,20 +1,68 @@
-{ fetchurl, stdenv, }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  installShellFiles,
+}:
 
 let
-  version = "latest";
-  hash = "sha256-K4CfC4pEF9FPdcxERJrMEAfwYCWmMtZrOT1Hw1zxC7I=";
-  # sha256 = "03x792gqgxfsiyd7m03p2hbdm4qnf4y6pdxr40qz5zf7a4rvmz58";
-  src = fetchurl {
-    url = "https://akeyless-cli.s3.us-east-2.amazonaws.com/cli/${version}/production/cli-linux-amd64";
-    hash = hash;
+  version = "1.143.0";
+
+  completionSrc = fetchurl {
+    url = "https://download.akeyless.io/Akeyless_Artifacts/Linux/CLI/akeyless.bash_completion";
+    hash = "sha256-ZNS8FWcBwu7QFtH7joJiAWctuA8Ny7SGXRmlBbrxaQo=";
+  };
+
+  srcs = {
+    x86_64-darwin = {
+      url = "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless";
+      hash = "sha256-O4rFqbTnVbCRvk7V6CN0CHd78pZoFekhjb5Abkyltco=";
+    };
+    aarch64-darwin = {
+      url = "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless-arm";
+      hash = "sha256-OaAO9y2G+5Q4y3Cjpm4aYyD/M6OAU8fpReVh5IJpzsw=";
+    };
+    x86_64-linux = {
+      url = "https://download.akeyless.io/Akeyless_Artifacts/Linux/CLI/akeyless";
+      hash = "sha256-4KsH8YWPGhot3XyvVJFStRj5A8FYhpDEsnpgqU6JhIY=";
+    };
   };
 in
 stdenv.mkDerivation {
-  name = "akeyless-cli-${version}";
-  src = src;
-  phases = [ "installPhase" ];
+  pname = "akeyless";
+  inherit version;
+
+  src = fetchurl (
+    srcs.${stdenv.hostPlatform.system} or (throw "unsupported system: ${stdenv.hostPlatform.system}")
+  );
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [
+    installShellFiles
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
   installPhase = ''
-    mkdir -p $out/bin
-    install -m755 ${src} $out/bin/akeyless
+    runHook preInstall
+    install -Dm755 $src $out/bin/akeyless
+    installShellCompletion --bash --name akeyless ${completionSrc}
+    runHook postInstall
   '';
+
+  passthru.updateScript = ./update.sh;
+
+  meta = {
+    description = "Akeyless CLI";
+    homepage = "https://www.akeyless.io";
+    license = lib.licenses.unfree;
+    mainProgram = "akeyless";
+    maintainers = with lib.maintainers; [ frantathefranta ];
+    platforms = lib.attrNames srcs;
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+  };
 }

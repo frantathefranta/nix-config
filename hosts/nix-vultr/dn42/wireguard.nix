7fe5209326eba3d0793393078adbcc90b33b8b38
Franta Bartik <fb@franta.us>
2026-06-26 23:49:11 -0400

    Fixed endpoint port

--- hosts/nix-vultr/dn42/wireguard.nix
+++ hosts/nix-vultr/dn42/wireguard.nix
@@ -12,7 +12,7 @@ in
   services.custom-wireguard.interfaces = {
     "ibgp_us1" = {
       listenPort = 24001;
-      peerEndpoint = "us1.dn42.franta.us:21033";
+      peerEndpoint = "us1.dn42.franta.us:24003";
       peerPublicKey = "us1UyUrDb+609N10WRholfb4Q6XuOGvx23uPMQbJdTg=";
       localAddressV6 = "fe80::${subnet}:1033/128";
       peerAddressV6 = "fe80::3:1033/128";

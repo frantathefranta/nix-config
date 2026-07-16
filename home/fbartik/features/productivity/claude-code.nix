05618ce3826dbb02b4bb79e6f8354d10f8a8dc74
Franta Bartik <fb@franta.us>
2026-07-16 14:15:26 -0400

    Removed claude sandbox

--- home/fbartik/features/productivity/claude-code.nix
+++ home/fbartik/features/productivity/claude-code.nix
@@ -3,16 +3,5 @@
   programs.claude-code = {
     enable = true;
     package = pkgs.unstable.claude-code;
-    settings = {
-      sandbox = {
-        enabled = true;
-        filesystem = {
-          allowRead = [ "." ];
-          denyRead = [ "~/" ];
-          allowWrite = [ "." ];
-          denyWrite = [ "/" ];
-        };
-      };
-    };
   };
 }

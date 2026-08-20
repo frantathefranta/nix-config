e43fb7498d8e13897c37468aea844d672590dcac
Franta Bartik <fb@franta.us>
2026-08-20 09:57:50 -0400

    Added rev to pi-subagents

--- home/fbartik/features/productivity/pi/packages/pi-subagents.nix
+++ home/fbartik/features/productivity/pi/packages/pi-subagents.nix
@@ -8,7 +8,7 @@
     src = pkgs.fetchFromGitHub {
       owner = "nicobailon";
       repo = "pi-subagents";
-      rev = "";
+      rev = "10f69cdfd1ec384a3d6079b136b88970d79ec09a";
       hash = "sha256-jCvTUW6u7eHb1+2/qtjGAID5WkxXhjYA4k1HohOCIRQ=";
     };
     npmDepsHash = "sha256-g9Da56i6YcYZPqR4QUjqG7AFDM5ktYXuns0YknotyPM=";

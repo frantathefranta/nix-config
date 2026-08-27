f76caa13b1ece93f9f3ad138e316a62236051a16
Franta Bartik <fb@franta.us>
2026-08-26 22:46:48 -0400

    Added ACME for WebDAV

--- hosts/nixos-firewall/services/webdav.nix
+++ hosts/nixos-firewall/services/webdav.nix
@@ -4,13 +4,27 @@
 }:
 
 {
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "fb@franta.us";
+    certs."webdav.franta.us" = {
+      dnsProvider = "cloudflare";
+      dnsResolver = "1.1.1.1:53";
+      environmentFile = config.sops.secrets."acme/cloudflare".path;
+      group = config.services.webdav.group;
+    };
+  };
+
   services.webdav = {
     enable = true;
     settings = {
       address = "0.0.0.0";
       port = 8080;
+      tls = true;
+      cert = "${config.security.acme.certs."webdav.franta.us".directory}/cert.pem";
+      key = "${config.security.acme.certs."webdav.franta.us".directory}/key.pem";
       directory = "/srv/public";
-      permissions = "R";
+      permissions = "CRUD";
       users = [
         {
           username = "{env}ENV_USERNAME";
@@ -19,6 +33,14 @@
       ];
     };
     environmentFile = config.sops.secrets."webdav-env".path;
+  };
+  networking.nftables.firewall.rules.webdav = {
+    from = [
+      "local_interfaces"
+      "wg"
+    ];
+    to = [ "fw" ];
+    allowedTCPPorts = [ 8080 ];
   };
   sops.secrets."webdav-env".sopsFile = ../secrets.yaml;
 }

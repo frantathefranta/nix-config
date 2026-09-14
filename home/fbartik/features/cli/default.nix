dc71f203076f5326a187de4c26e57fec50c11951
Franta Bartik <fb@franta.us>
2026-09-14 12:54:58 -0400

    Removed devenv

--- home/fbartik/features/cli/default.nix
+++ home/fbartik/features/cli/default.nix
@@ -14,9 +14,7 @@
   home.packages =
     with pkgs;
     [
-      cachix # Cachix CLI client
       comma # Install and run programs by sticking a , before them
-      devenv
 
       bc # Calculator
       bottom # System viewer
